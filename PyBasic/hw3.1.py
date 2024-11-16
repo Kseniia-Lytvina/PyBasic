9d56ff8c3d9a5b5d80ee3a5504876a20572ddcf5
Kseniia-Lytvina <kseniia.lytvina@gmail.com>
2024-11-16 20:58:12 +0100

    Create hw3.1.py

---
+++ PyBasic/hw3.1.py
@@ -0,0 +1,21 @@
+num1 = float(input("Введіть перше число: "))
+operation = input("Введіть дію (+, -, *, /): ")
+num2 = float(input("Введіть друге число: "))
+
+if operation == "+":
+    result = num1 + num2
+    print("Результат:", result)
+elif operation == "-":
+    result = num1 - num2
+    print("Результат:", result)
+elif operation == "*":
+    result = num1 * num2
+    print("Результат:", result)
+elif operation == "/":
+    if num2 != 0:
+        result = num1 / num2
+        print("Результат:", result)
+    else:
+        print("Помилка: ділення на 0!")
+else:
+    print("Помилка: невідома дія!")
